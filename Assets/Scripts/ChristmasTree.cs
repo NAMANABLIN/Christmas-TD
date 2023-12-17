632a52ce252325fadedb7437eeb8e7a0f4aed573
Vedmich <78676683+RetroVedmich@users.noreply.github.com>
2023-12-17 18:31:54 +0500

    Health Bar and GameOver menu

--- Assets/Scripts/ChristmasTree.cs
+++ Assets/Scripts/ChristmasTree.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tree : MonoBehaviour
 {
+    public float TreeHealth = 500f;
+    [SerializeField] private Slider _slider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Tree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _slider.value = TreeHealth / 500;
     }
 }
